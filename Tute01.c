3112cd5a64d067b706a9ebeed33f4e2c688a9696
Ishara Madushan <99213946+IT21183904@users.noreply.github.com>
2022-02-08 06:54:55 +0000

    Tut 01: exercise 1 done

--- Tute01.c
+++ Tute01.c
@@ -1,11 +1,17 @@
-/* Exercise 1 - Calculations
+#include<stdio.h>
+int main(void)
+{
+  int x,y,total=0;
+  float avg;
 
-   Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
+  printf("Enter the first mark :");
+  scanf("%d",&x);
+  printf("Enter the second mark :");
+  scanf("%d",&y);
 
-#include <stdio.h>
+  total= x+y;
+  avg = total/2;
 
-int main() {
-  
-  return 0;
+  printf("Average is :%.2f",avg);
+
 }
-
