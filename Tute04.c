1ab066d4f1fc0cd29aabe3343b3b485f15a4f173
IT21183904 <99213946+IT21183904@users.noreply.github.com>
2022-02-15 00:47:57 +0530

    Update Tute04.c

--- Tute04.c
+++ Tute04.c
@@ -5,38 +5,39 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 
 Do not change the code given in the main() function when you are implementing your solution.*/
 
-#include <stdio.h>
+#include<stdio.h>
+int minimum(int , int);
+int maximum(int, int);
+int multiply(int,int);
 
-int main(void) {
-   int n1=0, n2=0;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &n1);
-
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &n2);
-
-   printf("Minimum is :%d\n",minimumm(n1,n2));
-   printf("Maximum is :%d\n",maximumm(n1,n2));
-   printf("Multiply is %d\n",multiplyy(n1,n2));
+int main()
+{
+int no1,no2;
+printf("Enter a value for no 1 : "); 
+scanf("%d", &no1);
+printf("Enter a value for no 2 : "); 
+scanf("%d", &no2);
+printf("%d ", minimum(no1, no2)); 
+printf("%d ", maximum(no1, no2)); 
+printf("%d ", multiply(no1, no2)); 
+return 0;
 }
+int minimum(int a, int b){
+  if(a>b)
+  return b;
+  else
+  return a;
+  
+  }
+  int maximum (int a, int b){
+    if(a>b)
+    return a;
+    else
+    return b;
+  }
+  int multiply(int a, int b){
+    return a*b;
+  }
+  
 
-   int minimumm(int no1,int no2)
-   {
-     if(no1>no2)
-     return no2;
-     else
-     return no1;
-   }
-   int maximumm(int no1,int no2)
-   {
-     if(no1<no2)
-     return no2;
-     else
-     return no1;
-
-   }
-   int multiplyy(int no1,int no2)
-   {
-     return no1*no2;
-   }
    
